43225f3cf6975550bfc83a8b5d195468d7007c6b
jdragon <jdragon9527@gmail.com>
2025-04-09 20:17:53 +0800

    feat: 修复本地模式图片对比错误。鉴权去除。

--- core/image_comparator/DynamicSizeImageComparator.py
+++ core/image_comparator/DynamicSizeImageComparator.py
@@ -1,12 +1,14 @@
+from core.image_comparator.LocalImageComparator import LocalImageComparator
 from log import LogFactory
 
 
-class DynamicSizeImageComparator:
+class DynamicSizeImageComparator(LocalImageComparator):
     """
         可动态模糊匹配的网络图片对比
     """
 
     def __init__(self, base_path, screen_taker, base_image_comparator):
+        super().__init__(base_path)
         self.image_cache = {}
         self.logger = LogFactory.getLogger(self.__class__)
         self.base_path = base_path
@@ -16,7 +18,7 @@ class DynamicSizeImageComparator:
     def compare_with_path(self, path, images, lock_score, discard_score):
         path = self.base_path + path
         image_info_arr = [image_info.split() for image_info in
-                          self.base_image_comparator.read_file_from_url_and_cache(path, "list.txt")]
+                          super().read_file_from_url_and_cache(path, "list.txt")]
         select_name, score_temp = self.match_template(path, image_info_arr, threshold=discard_score)
         return select_name, score_temp
 
@@ -26,7 +28,7 @@ class DynamicSizeImageComparator:
             image_path = path + image_path
             box = (int(x), int(y), int(w), int(h))
             img = self.screen_taker.get_images_from_bbox([box])[0]
-            score = self.base_image_comparator.compare_image(img, image_path)
+            score = super().compare_image(img, image_path)
             if score > threshold:
                 return image_info[0].split(".")[0], score
         return "", 0.0
@@ -38,4 +40,4 @@ class DynamicSizeImageComparator:
             image_path = base_path + image_path
         else:
             image_path = line_content
-        self.base_image_comparator.cache_image("", image_path)
+        super().cache_image("", image_path)
